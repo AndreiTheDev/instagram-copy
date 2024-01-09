cc500f1b334ec4e2db2a96d808f3f354a1fd0ee2
Emanuel Lamba <emanuellamba13@gmail.com>
2024-01-09 12:17:38 +0200

    feat: create firebase functions folder and integrate them inside the app, added 3 new functions related to app authentication, also updated ios Podfile.lock

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const authTrigger = require('./auth_functions/auth_triggers');
-const postsignupdetails = require('./auth_functions/auth_callable');
+const authTrigger = require("./auth_functions/auth_triggers");
+const postsignupdetails = require("./auth_functions/auth_callable");
 exports.authTrigger = authTrigger;
 exports.postsignupdetails = postsignupdetails;
