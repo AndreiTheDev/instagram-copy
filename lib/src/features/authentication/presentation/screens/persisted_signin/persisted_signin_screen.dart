58b04379ebc451e5c5b1567198990e61329300f1
Emanuel Lamba <emanuellamba13@gmail.com>
2024-01-15 10:49:24 +0200

    feat: make signin screens pop close the app if they are last route

--- lib/src/features/authentication/presentation/screens/persisted_signin/persisted_signin_screen.dart
+++ lib/src/features/authentication/presentation/screens/persisted_signin/persisted_signin_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_decorated_box
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../../constants/exports.dart';
@@ -29,64 +30,70 @@ class _PersistedSignInScreenState extends ConsumerState<PersistedSignInScreen> {
     final persistedUsers = ref.watch(persistedUsersControllerProvider);
 
     return switch (persistedUsers) {
-      AsyncData(:final value) => Scaffold(
-          resizeToAvoidBottomInset: false,
-          body: GradientDecoration(
-            child: Padding(
-              padding: const EdgeInsets.all(xsSize),
-              child: Column(
-                children: [
-                  CustomAppBar(
-                    buttonRight: IconButton(
-                      iconSize: 28,
-                      onPressed: () async {
-                        if (value.length == 1) {
-                          await PersistedSingleSettingsRoute($extra: value[0])
-                              .push(context);
-                        }
-                        if (value.length > 1 && context.mounted) {
-                          await const PersistedMultiSettingsRoute()
-                              .push(context);
-                        }
-                      },
-                      icon: const Icon(Icons.settings_outlined),
-                    ),
-                  ),
-                  xsSeparator,
-                  Image.asset(igLogoPath),
-                  largeSeparator,
-                  PersistedUsersLayout(usersList: value),
-                  if (value.length == 1) mediumSeparator,
-                  if (value.length == 1)
-                    Consumer(
-                      builder: (final context, final ref, final child) =>
-                          AuthButton(
-                        text: 'Log in',
-                        callback: () async {},
+      AsyncData(:final value) => PopScope(
+          canPop: false,
+          onPopInvoked: (didPop) async {
+            await SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+          },
+          child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            body: GradientDecoration(
+              child: Padding(
+                padding: const EdgeInsets.all(xsSize),
+                child: Column(
+                  children: [
+                    CustomAppBar(
+                      buttonRight: IconButton(
+                        iconSize: 28,
+                        onPressed: () async {
+                          if (value.length == 1) {
+                            await PersistedSingleSettingsRoute($extra: value[0])
+                                .push(context);
+                          }
+                          if (value.length > 1 && context.mounted) {
+                            await const PersistedMultiSettingsRoute()
+                                .push(context);
+                          }
+                        },
+                        icon: const Icon(Icons.settings_outlined),
                       ),
                     ),
-                  xsSeparator,
-                  AuthOutlinedButton(
-                    text: 'Log in another account',
-                    callback: () async =>
-                        const PersistedSignInRoute().push(context),
-                  ),
-                  const Expanded(child: SizedBox()),
-                  AuthOutlinedButton(
-                    text: 'Create a new account',
-                    callback: () async {
-                      await const PersistedCompleteNameRoute().push(context);
-                    },
-                    textColor: loginBlueColor,
-                    borderColor: loginBlueColor,
-                  ),
-                  minuscleSeparator,
-                  Image.asset(
-                    metaLogoPath,
-                    width: 64,
-                  ),
-                  if (isIos) xsSeparator,
-                ],
+                    xsSeparator,
+                    Image.asset(igLogoPath),
+                    largeSeparator,
+                    PersistedUsersLayout(usersList: value),
+                    if (value.length == 1) mediumSeparator,
+                    if (value.length == 1)
+                      Consumer(
+                        builder: (final context, final ref, final child) =>
+                            AuthButton(
+                          text: 'Log in',
+                          callback: () async {},
+                        ),
+                      ),
+                    xsSeparator,
+                    AuthOutlinedButton(
+                      text: 'Log in another account',
+                      callback: () async =>
+                          const PersistedSignInRoute().push(context),
+                    ),
+                    const Expanded(child: SizedBox()),
+                    AuthOutlinedButton(
+                      text: 'Create a new account',
+                      callback: () async {
+                        await const PersistedCompleteNameRoute().push(context);
+                      },
+                      textColor: loginBlueColor,
+                      borderColor: loginBlueColor,
+                    ),
+                    minuscleSeparator,
+                    Image.asset(
+                      metaLogoPath,
+                      width: 64,
+                    ),
+                    if (isIos) xsSeparator,
+                  ],
+                ),
               ),
             ),
           ),
--- lib/src/features/authentication/presentation/screens/signin_screen.dart
+++ lib/src/features/authentication/presentation/screens/signin_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -45,109 +46,121 @@ class _SignInScreenState extends ConsumerState<SignInScreen> {
   Widget build(final BuildContext context) {
     final bool isIos = Theme.of(context).platform == TargetPlatform.iOS;
     final isPersisted = ref.watch(isPersistedFlowProvider);
-    print('$isPersisted');
+    // getLogger(SignInScreen).d('$isPersisted');
 
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: GradientDecoration(
-        child: Padding(
-          padding: const EdgeInsets.all(xsSize),
-          child: Column(
-            children: [
-              if (isPersisted.hasValue && isPersisted.value!)
-                CustomAppBar(
-                  buttonLeft: CustomBackButton(onPressed: context.pop),
-                )
-              else
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) async {
+        if (isPersisted.hasValue && isPersisted.value!) {
+          context.pop();
+          return;
+        }
+        await SystemChannels.platform.invokeListMethod('SystemNavigator.pop');
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: GradientDecoration(
+          child: Padding(
+            padding: const EdgeInsets.all(xsSize),
+            child: Column(
+              children: [
+                if (isPersisted.hasValue && isPersisted.value!)
+                  CustomAppBar(
+                    buttonLeft: CustomBackButton(onPressed: context.pop),
+                  )
+                else
+                  xlSeparator,
+                const Text(
+                  'English',
+                  style: TextStyle(
+                    fontSize: smallText,
+                  ),
+                ),
                 xlSeparator,
-              const Text(
-                'English',
-                style: TextStyle(
-                  fontSize: smallText,
-                ),
-              ),
-              xlSeparator,
-              Image.asset(igLogoPath),
-              xlSeparator,
-              AuthTextField(
-                textController: _emailController,
-                labelText: 'Username, email adress or phone number',
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    _emailController.value = TextEditingValue.empty;
-                  },
-                  icon: const Icon(
-                    Icons.close,
-                    size: 30,
-                    color: Color(0xff6f7a85),
+                Image.asset(igLogoPath),
+                xlSeparator,
+                AuthTextField(
+                  textController: _emailController,
+                  labelText: 'Username, email adress or phone number',
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      _emailController.value = TextEditingValue.empty;
+                    },
+                    icon: const Icon(
+                      Icons.close,
+                      size: 30,
+                      color: Color(0xff6f7a85),
+                    ),
                   ),
                 ),
-              ),
-              xsSeparator,
-              AuthTextField(
-                textController: _passwordController,
-                labelText: 'Password',
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      obscurePassword = !obscurePassword;
-                    });
-                  },
-                  icon: Icon(
-                    obscurePassword
-                        ? Icons.visibility_off_outlined
-                        : Icons.visibility_outlined,
-                    size: 30,
-                    color: const Color(0xff6f7a85),
+                xsSeparator,
+                AuthTextField(
+                  textController: _passwordController,
+                  labelText: 'Password',
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        obscurePassword = !obscurePassword;
+                      });
+                    },
+                    icon: Icon(
+                      obscurePassword
+                          ? Icons.visibility_off_outlined
+                          : Icons.visibility_outlined,
+                      size: 30,
+                      color: const Color(0xff6f7a85),
+                    ),
+                  ),
+                  iconVisibility: obscurePassword,
+                ),
+                xsSeparator,
+                Consumer(
+                  builder: (final context, final ref, final child) =>
+                      AuthButton(
+                    text: 'Log in',
+                    callback: () async {
+                      await ref.read(authControllerProvider).signIn(
+                            _emailController.text,
+                            _passwordController.text,
+                          );
+                    },
                   ),
                 ),
-                iconVisibility: obscurePassword,
-              ),
-              xsSeparator,
-              Consumer(
-                builder: (final context, final ref, final child) => AuthButton(
-                  text: 'Log in',
+                xsSeparator,
+                AuthTextButton(
+                  text: 'Forgotten password?',
                   callback: () async {
-                    await ref.read(authControllerProvider).signIn(
-                          _emailController.text,
-                          _passwordController.text,
-                        );
+                    if (isPersisted.hasValue && isPersisted.value!) {
+                      await const PersistedRecoverPasswordRoute().push(context);
+                    } else {
+                      await const RecoverPasswordRoute().push(context);
+                    }
                   },
                 ),
-              ),
-              xsSeparator,
-              AuthTextButton(
-                text: 'Forgotten password?',
-                callback: () async {
-                  if (isPersisted.hasValue && isPersisted.value!) {
-                    await const PersistedRecoverPasswordRoute().push(context);
-                  } else {
-                    await const RecoverPasswordRoute().push(context);
-                  }
-                },
-              ),
-              const Expanded(
-                child: SizedBox(),
-              ),
-              AuthOutlinedButton(
-                text: 'Create a new account',
-                callback: () async {
-                  if (isPersisted.hasValue && isPersisted.value!) {
-                    const PersistedCompleteNameRoute().pushReplacement(context);
-                  } else {
-                    await const CompleteNameRoute().push(context);
-                  }
-                },
-                textColor: const Color(0xff0665bd),
-                borderColor: const Color(0xff2169b1),
-              ),
-              xsSeparator,
-              Image.asset(
-                metaLogoPath,
-                width: 64,
-              ),
-              if (isIos) xsSeparator,
-            ],
+                const Expanded(
+                  child: SizedBox(),
+                ),
+                AuthOutlinedButton(
+                  text: 'Create a new account',
+                  callback: () async {
+                    if (isPersisted.hasValue && isPersisted.value!) {
+                      const PersistedCompleteNameRoute()
+                          .pushReplacement(context);
+                    } else {
+                      await const CompleteNameRoute().push(context);
+                    }
+                  },
+                  textColor: const Color(0xff0665bd),
+                  borderColor: const Color(0xff2169b1),
+                ),
+                xsSeparator,
+                Image.asset(
+                  metaLogoPath,
+                  width: 64,
+                ),
+                if (isIos) xsSeparator,
+              ],
+            ),
           ),
         ),
       ),
